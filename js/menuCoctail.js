fd690f4c47cbdde264cef8b7344bf4a268871f16
EnoTanya <72948839+TanyaShcherba@users.noreply.github.com>
2021-11-19 08:38:34 +0300

    Add files via upload

--- js/menuCoctail.js
+++ js/menuCoctail.js
@@ -2,27 +2,27 @@
 
 let coctailStorage = new barHashStorage();
 
-coctailStorage.addValue(' Голубая лагуна',{
+coctailStorage.addValue('Голубая лагуна',{
     alcohol: true,
     recipe: "<br>"+"1. Водка Finlandia - 50 мл."+"<br>"+"2. Ликер блю кюрасао De Kuyper - 20 мл."+"<br>"+"3. Спрайт - 150 мл."+"<br>"+"4. Ананас - 30 г."+"<br>"+"5. Лед в кубиках - 200 г."
 });
 
-coctailStorage.addValue(' Мохито',{
+coctailStorage.addValue('Мохито',{
     alcohol: true,
     recipe: "<br>"+"1. Белый ром - 50 мл."+"<br>"+"2. Сахарный сироп - 15 мл."+"<br>"+"3. Содовая - 100 мл."+"<br>"+"4. Лайм - 80 г."+"<br>"+"5. Мята - 3 г."+"<br>"+"6. Дробленый лед - 200 г."
 });
 
-coctailStorage.addValue(' Джин тоник',{
+coctailStorage.addValue('Джин тоник',{
     alcohol: true,
     recipe: "<br>"+"1. Лондонский сухой джин - 50 мл."+"<br>"+"2. Тоник - 150 мл."+"<br>"+"3. Лайм - 20 г."+"<br>"+"4. Лед в кубиках - 180 г."
 });
 
-coctailStorage.addValue(' Кровавая Мэри',{
+coctailStorage.addValue('Кровавая Мэри',{
     alcohol: true,
     recipe: "<br>"+"1. Водка Finlandia - 50 мл."+"<br>"+"2. Томатный сок - 120 мл."+"<br>"+"3. Лимонный сок - 10 мл."+"<br>"+"4. Сельдерей - 15 г."+"<br>"+"5. Табаско соус красный - 1 мл."+"<br>"+"6. Лед в кубиках - 380 г."
 });
 
-coctailStorage.addValue(' Американо',{
+coctailStorage.addValue('Американо',{
     alcohol: true,
     recipe: "<br>"+"1. Красный вермут - 50 мл."+"<br>"+"2. Красный биттер Campari - 50 мл."+"<br>"+"3. Содовая - 50 мл."+"<br>"+"4. Апельсиновая цедра - 1 шт."+"<br>"+"5. Лед в кубиках - 180 г."
 });
